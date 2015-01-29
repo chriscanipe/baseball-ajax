54c2f2dbb4cef234d3c2d721d48cb5ef04920dbc
Chris Canipe <chris.canipe@wsj.com>
2015-01-28 22:06:56 -0600

    setting up example for class

--- js/script.js
+++ js/script.js
@@ -16,11 +16,46 @@ function getData() {
 
 function drawStuff() {
 	
-	$(".chart").html(playerData.name);
+	//The playerData variable contains our data.
+	//I'll create variables here to illustrate how each attibute is accessed
+	//I could also just do this inline below, but breaking it out here makes things a little clearer I think.
 
-	$.each(playerData.stats, function(i, item) {
-		$(".chart").append("<div class='avg'>"+item.AVG+"</div>");
-	});
+	var playerName = playerData.name;
+	var playerTeam = playerData.club;
+
+	//Creating a variable for our players stats, which is an array, or *list* of seasons.
+	var playerStats = playerData.stats;
+
+	//To access a season in our array, we'll identify it by its index number, or order in the array
+	//For example, the first season would be playerStats[0] (Remember, sequences are zero-based in JavaScript)
+	//But in order to access the last season, we need to know the index number of that season in the array.
+	//We could count just look at the JSON and count them, sure. But that's work. And we're laaaazy.
+	//So we'll do it with code:
+
+	var lastSeasonIndex = playerStats.length -1;
+
+	//.length tells us how many items are in our list. And since index values are zero based...
+	//... we're really looking for one less than the total number of items.
+	//So the most recent season is this:
+
+	var lastSeasonStats = playerData.stats[lastSeasonIndex];
+
+	var lastSeasonBattingAverage = lastSeasonStats.AVG;
+	var lastSeasonYear = lastSeasonStats.year;
+
+	//All that's left to do is write it to our page.
+	$(".chart").append("<h1>"+playerName+"</h1>");
+	$(".chart").append("<h3>"+playerTeam+"</h3>");
+	$(".chart").append("<p>In "+lastSeasonYear+", "+playerName+"'s batting average was "+lastSeasonBattingAverage);
+
+
+	//Me: Cool, right?
+	//You: Yep, pretty cool.
+
+
+	// $.each(playerData.stats, function(i, item) {
+	// 	$(".chart").append("<div class='avg'>"+item.AVG+"</div>");
+	// });
 
 }
 
